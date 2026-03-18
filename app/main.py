4eedeefa20fdc8d7da60f28f731676f69ee62461
Hamood-pixel <tcs166214@gmail.com>
2026-03-19 01:23:58 +0500

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -9,6 +9,11 @@ def main():
         command = input()
         if command.startswith("echo "):
             print(command[5:])
+        elif command.startswith("type "):
+            if(command[5:]== "echo" or command[5:] == "exit" or command[5:] == "type"):
+                print(f"{command[5:]} is a shell builtin")
+            else:
+                print(f"{command[5:]}: not found")
         elif command =="exit":
             break 
         else:
